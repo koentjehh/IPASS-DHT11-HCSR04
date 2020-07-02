806044e813f0c82681a0e4e9d5590d0bb07be777
Koen <60387717+koentjehh@users.noreply.github.com>
2020-07-02 19:08:16 +0200

    Update main.cpp

--- DHT11/example code/main.cpp
+++ DHT11/example code/main.cpp
@@ -4,20 +4,20 @@ int main( void )
 {
 	auto data_pin = hwlib::target::pin_in_out( hwlib::target::pins::d7 );
 
-	auto DHT11_sensor = DHT11(data_pin);
+	auto DHT11_sensor = DHT11( data_pin );
 	
-	hwlib::wait_ms(1000); // 1 second wait to enter stable state
+	hwlib::wait_ms( 1000 ); // a one-second wait to reach stable state
 	
 	while(true){
 		
-		DHT11_sensor.start_and_read();
+		DHT11_sensor.start_and_read(); // the sensor is started and the data will be collected
 		
-		int temperature = DHT11_sensor.temperature();
-		int humidity 	= DHT11_sensor.humidity();
+		int temperature = DHT11_sensor.temperature(); // obtain the collected temperature data
+		int humidity 	= DHT11_sensor.humidity();    // obtain the collected humidity data
 		
 		hwlib::cout << "Temperature: " << temperature << "*C\n"
 			    << "humidity   : " << humidity    << "%\n";
 		
-		hwlib::wait_ms(5000); // to get accurate results check every 5 seconds max.
+		hwlib::wait_ms( 5000 ); // a sensor interval of no less than 5 seconds is required to obtain accurate data
 	}
 }

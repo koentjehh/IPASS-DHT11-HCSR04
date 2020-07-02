f05e0f9a0b8962bfeab63ed062b63155f6506a82
Koen <60387717+koentjehh@users.noreply.github.com>
2020-07-02 19:00:46 +0200

    Update main.cpp

--- HC-SR04/example code/main.cpp
+++ HC-SR04/example code/main.cpp
@@ -7,7 +7,7 @@ int main()
 	auto trig_pin = target::pin_out( ::target::pins::d8 );
         auto echo_pin = target::pin_in ( ::target::pins::d9 );
 	
-	HCSR04 HCSR04_sensor(trig, echo);
+	HCSR04 HCSR04_sensor( trig, echo );
 	
 	int distance;
 
